4d118bf10bb7dd11cf6a16035d1e689c8521239f
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-01-26 10:51:54 +0200

    add meal modal

--- 02. Layout, Styling and User Input/02.Exercises/calorie-tracker/components/add-meal/AddMeal.jsx
+++ 02. Layout, Styling and User Input/02.Exercises/calorie-tracker/components/add-meal/AddMeal.jsx
@@ -1,4 +1,4 @@
-import { Modal, Text } from "react-native";
+import { View, Modal, Text, TextInput, StyleSheet } from "react-native";
 
 export default function AddMeal({
     visible,
@@ -7,8 +7,52 @@ export default function AddMeal({
     return (
         <Modal
             onRequestClose={onClose}
+            transparent
+            style={{height: 300}}
         > 
-            <Text>Add meal</Text>
+            <View style={styles.overlay}>
+
+                <View style={styles.modal}>
+                    <Text>Add meal</Text>
+
+                    <View>
+                        <Text>Food Name</Text>
+                        <TextInput placeholder="e.g. Pizza slice" />
+                    </View>
+
+                    <View>
+                        <Text>Calories</Text>
+                        <TextInput placeholder="0" keyboardType="number-pad"/>
+                    </View>
+
+                    <View>
+                        <Text></Text>
+                        <Text></Text>
+                    </View>
+
+                </View>
+
+            </View>
+
         </Modal>
     )
-}
+};
+
+export const styles = StyleSheet.create({ 
+    overlay: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        alignItems: 'center',
+    },
+    modal: {
+        width: 300,
+        height: 200,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        padding: 16,
+        justifyContent: 'space-between'
+        
+    }
+
+})
