d30291c4191064e986b812b0103baba3d854d22c
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-01-27 00:26:48 +0200

    add data for items

---
+++ 03. Navigation/01.Lecture/react-navigation/data.js
@@ -0,0 +1,7 @@
+export const items = [
+    {id: 1, name: 'first', description: 'Description for first'},
+    {id: 2, name: 'Second', description: 'Description for Second'},
+    {id: 3, name: 'Third', description: 'Description for Third'},
+    {id: 4, name: 'Fourth', description: 'Description for Fourth'},
+    {id: 5, name: 'Fifth', description: 'Description for Fifth'},
+]
