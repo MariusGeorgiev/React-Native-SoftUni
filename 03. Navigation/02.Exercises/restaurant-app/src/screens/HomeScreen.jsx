cef84fa5447234bbaac494538a28af7cedbd208e
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-01-28 14:51:56 +0200

    add header to home tab and his style

--- 03. Navigation/02.Exercises/restaurant-app/src/screens/HomeScreen.jsx
+++ 03. Navigation/02.Exercises/restaurant-app/src/screens/HomeScreen.jsx
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { featuredItems } from '../data/menuItems';
+import Card from '../components/Card';
 
 export default function HomeScreen() {
 
     return (
-        <View style={styles.container}>
-          <Text>Restaurant App</Text>
-          
-        </View>
+      <ScrollView>
+            <View style={styles.header}>
+                <Text style={styles.restaurantName}>Tasty Bites</Text>
+                <View style={styles.headerInfo}>
+                    <Text style={styles.infoText}>⭐ 4.8 Rating</Text>
+                    <Text style={styles.infoDot}>●</Text>
+                    <Text style={styles.infoText}>⏱️ 25-35 min</Text>
+                </View>
+                <Text style={styles.tagline}>Fresh & Delicious Food Delivered Fast</Text>
+            </View>
+
+            {/* Featured Section */}
+       
+
+            {/* Category Section */}
+
+        </ScrollView>
     );
 }
 
@@ -14,7 +29,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    backgroundColor: '#007AFF',
+    padding: 24,
+    paddingTop: 16,
+    borderBottom: 28,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
+
+  },
+  restaurantName: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 8,
+  },
+  headerInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  infoText: {
+    fontSize: 14,
+    color: '#fff',
+    opacity: 0.9,
+  },
+  infoDot: {
+    fontSize: 14,
+    color: '#fff',
+    opacity: 0.6,
+    marginHorizontal: 8,
+  },
+  tagline: {
+    fontSize: 14,
+    color: '#fff',
+    opacity: 0.8,
+  },
+
 });
