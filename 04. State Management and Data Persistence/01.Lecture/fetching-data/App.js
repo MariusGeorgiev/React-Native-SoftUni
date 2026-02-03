35860df17b587dfeb8c0ea3dbc8afb2a3859aacf
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-03 20:06:17 +0200

    finish app.js

--- 04. State Management and Data Persistence/01.Lecture/fetching-data/App.js
+++ 04. State Management and Data Persistence/01.Lecture/fetching-data/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, ScrollView, FlatList } from 'react-native';
+import request from './request';
+import CharacterItem from './components/CharacterItem';
 
 export default function App() {
+  const { characters, loading, error } = useCharacters();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>Star Wars Characters</Text>
+
+      <View>
+        {loading
+          ? <ActivityIndicator size="large" color="#0000ff" />
+          : <FlatList
+              data={characters}
+              renderItem={({item}) => <CharacterItem {...item} />}
+              keyExtractor={(item, index) => index.toString() }
+              contentContainerStyle={{ paddingBottom: 50 }}
+          />
+        }
+        {error && <Text style={{ color: 'red' }}>Error fetching data</Text>}
+      </View>
     </View>
   );
 }
@@ -13,8 +31,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
+
+function useCharacters() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // fetch('https://swapi.dev/api/people')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     setCharacters(data.results);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching data:', error);
+    //   });
+
+    request.get('/people')
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error.message);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return { characters, loading, error };
+}
