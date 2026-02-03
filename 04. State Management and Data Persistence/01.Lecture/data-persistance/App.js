d0ed766a7c873890aa958f64f09f384f9f8c0794
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-03 20:07:43 +0200

    complete app.js

--- 04. State Management and Data Persistence/01.Lecture/data-persistance/App.js
+++ 04. State Management and Data Persistence/01.Lecture/data-persistance/App.js
@@ -1,11 +1,62 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    loadHandler();
+  }, []);
+
+  const saveHandler = async () => {
+    try {
+      await AsyncStorage.setItem('text', text);
+
+      setText('');
+    } catch (err) {
+      alert('Cannot save to storage...');
+    }
+  }
+
+  const loadHandler = async () => {
+    try {
+      const loadedText = await AsyncStorage.getItem('text');
+
+      setText(loadedText);
+    } catch (err) {
+      alert('Cannot read from storage');
+    }
+  }
+
+  const clearHandler = async () => {
+    try {
+      // await AsyncStorage.clear();
+      await AsyncStorage.removeItem('text');
+
+      setText('');
+    } catch (err) {
+      alert('Canot clear the storage');
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Data Persistance</Text>
+
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder='Text to save...'
+      />
+
+      <View style={{ flexDirection: 'row', gap: 10 }}>
+        <Button title="Save" onPress={saveHandler} />
+        <Button title="Load" onPress={loadHandler} />
+        <Button title="Clear" onPress={clearHandler} />
+      </View>
     </View>
   );
 }
@@ -13,6 +64,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
