8f2a793d37bee19383950ba9122f41df0c8af489
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-09 12:20:18 +0200

    update app navigation

--- 04. State Management and Data Persistence/02.Exercises/restaurant-app-from-last-exercise/src/App.jsx
+++ 04. State Management and Data Persistence/02.Exercises/restaurant-app-from-last-exercise/src/App.jsx
@@ -2,13 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import RootNavigator from './navigation/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native'
+import CartProvider from './contexts/cart/CartProvider';
 
 export default function App() {
   return (
         <NavigationContainer>
               <StatusBar style="auto" />
               {/* <SafeAreaView> */}
+              <CartProvider>
                 <RootNavigator />
+              </CartProvider>
               {/* </SafeAreaView> */}
         </NavigationContainer> 
   );
