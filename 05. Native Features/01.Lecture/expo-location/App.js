05729c449555e95172cc77a60152b2a3d2afd228
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-11 01:23:43 +0200

    finish app get location

--- 05. Native Features/01.Lecture/expo-location/App.js
+++ 05. Native Features/01.Lecture/expo-location/App.js
@@ -1,13 +1,48 @@
 import { getCurrentPositionAsync, LocationAccuracy, useForegroundPermissions, reverseGeocodeAsync, geocodeAsync } from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [status, requestPermission] = useForegroundPermissions();
+  const [pending, setPending] = useState(false);
+
+  if (!status) {
+    return <ActivityIndicator />;
+  }
+
+  if (!status.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>Location permission is required to use this app.</Text>
+        <Button title="Grant Permission" onPress={requestPermission} />
+      </View>
+    );
+  }
+
+  const getLocationHandler = async () => {
+    try {
+      setPending(true);
+      const location = await getCurrentPositionAsync({ accuracy: LocationAccuracy.BestForNavigation });
+      console.log(location);
+      // const address = await reverseGeocodeAsync(location.coords);
+      const address = await geocodeAsync("bul. Tsarigradsko shose 66, Sofia");
+      console.log(address);
+    } catch (error) {
+      console.log('Error getting location:', error);
+    } finally {
+      setPending(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={getLocationHandler}>
+        {pending
+          ? <ActivityIndicator />
+          : <Text style={{ backgroundColor: 'blue', color: 'white', padding: 10, borderRadius: 5 }}>Get Current Location</Text>}
+      </TouchableOpacity>
     </View>
   );
 }
