c4c94cba58b2be51e552fd104ce27c51a18c79e8
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-11 00:58:06 +0200

    add location imports

--- 05. Native Features/01.Lecture/expo-location/App.js
+++ 05. Native Features/01.Lecture/expo-location/App.js
@@ -1,4 +1,6 @@
+import { getCurrentPositionAsync, LocationAccuracy, useForegroundPermissions, reverseGeocodeAsync, geocodeAsync } from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
