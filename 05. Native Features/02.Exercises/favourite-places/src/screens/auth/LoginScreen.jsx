f26b7fb21bfd5d74c74baff309bdb5f700e4cbbd
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-11 05:03:54 +0200

    add react navigation

---
+++ 05. Native Features/02.Exercises/favourite-places/src/screens/auth/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import {
+    View,
+    Text
+} from 'react-native';
+
+export default function LoginScreen() {
+    return (
+    <View >
+        <Text style={{fontSize: 16, color: 'black'}}>Login Screen</Text>
+    </View>
+    )
+}
