965342daefeafc9dee41db96c0f647aefe13f814
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-12 07:30:59 +0200

    import auth provider in app.js

--- 05. Native Features/02.Exercises/favourite-places/src/App.js
+++ 05. Native Features/02.Exercises/favourite-places/src/App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
+import { AuthProvider } from './contexts/auth/AuthProvider';
 
 export default function App() {
   return (
     <NavigationContainer>
+
       <StatusBar style="auto" />
+
+      <AuthProvider>
+
+        <AppNavigator />
+
+      </AuthProvider>
       
-      <AppNavigator />
-  
     </NavigationContainer>
   );
 }
