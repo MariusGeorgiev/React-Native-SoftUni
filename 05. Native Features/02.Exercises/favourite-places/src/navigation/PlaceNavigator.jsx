0c8b739ad9daa49133137c5f60b3215e58debec7
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-13 23:37:31 +0200

    up navigation for another place screens

--- 05. Native Features/02.Exercises/favourite-places/src/navigation/PlaceNavigator.jsx
+++ 05. Native Features/02.Exercises/favourite-places/src/navigation/PlaceNavigator.jsx
@@ -1,8 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/places/HomeScreen.jsx";
-// import { CreatePlaceScreen } from "../screens/places/CreatePlaceScreen.jsx";
-// import ListPlaceScreen from "../screens/places/ListPlaceScreen.jsx";
-// import PlaceDetailsScreen from "../screens/places/PlaceDetailsScreen.jsx";
+import { CreatePlaceScreen } from "../screens/places/CreatePlaceScreen.jsx";
+import ListPlaceScreen from "../screens/places/ListPlaceScreen.jsx";
+import PlaceDetailsScreen from "../screens/places/PlaceDetailsScreen.jsx";
 
 export default function PlaceNavigator() {
     const Stack = createNativeStackNavigator();
@@ -11,9 +11,9 @@ export default function PlaceNavigator() {
         <Stack.Navigator>
             {/* Define your screens here */}
             <Stack.Screen name="Home" component={HomeScreen}  options={{title: 'Home'}}/>
-            {/* <Stack.Screen name="CreatePlace" component={CreatePlaceScreen} options={{title: 'Create Place'}} />
+            <Stack.Screen name="CreatePlace" component={CreatePlaceScreen} options={{title: 'Create Place'}} />
             <Stack.Screen name="ListPlaces" component={ListPlaceScreen}  options={{title: 'List Places'}}/>
-            <Stack.Screen name="PlaceDetails" component={PlaceDetailsScreen}  options={{title: 'Place Details'}}/> */}
+            <Stack.Screen name="PlaceDetails" component={PlaceDetailsScreen}  options={{title: 'Place Details'}}/>
         </Stack.Navigator>
     );
 }
