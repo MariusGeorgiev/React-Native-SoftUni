81a554db4f7e87fb7ce6832eb6c5e814ad35ced0
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-15 15:48:06 +0200

    up nav

--- 06. Advanced Styling and Basic Animations/01.Lecture/react-native-gestures/navigation/MainNavigator.jsx
+++ 06. Advanced Styling and Basic Animations/01.Lecture/react-native-gestures/navigation/MainNavigator.jsx
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import TapGesture from "../screens/TapGesture.jsx";
 import LongPressGesture from "../screens/LongPressGesture.jsx";
+import FlingGesture from "../screens/FlingGesture.jsx";
 
 
 export default function MainNavigator() {
@@ -11,6 +12,7 @@ export default function MainNavigator() {
 
             <Drawer.Screen name="TapGesture" component={TapGesture} options={{ title: 'Tap Gesture' }} />
             <Drawer.Screen name="LongPressGesture" component={LongPressGesture} options={{ title: 'Long Press' }} />
+            <Drawer.Screen name="FlingGesture" component={FlingGesture} options={{ title: 'Fling Gesture' }} />
    
         </Drawer.Navigator>
     );
