41f0a9bee98ef6d5bb6ddb3f899b3f42654866ed
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2026-02-15 15:49:05 +0200

    add in nav pan gesture

--- 06. Advanced Styling and Basic Animations/01.Lecture/react-native-gestures/navigation/MainNavigator.jsx
+++ 06. Advanced Styling and Basic Animations/01.Lecture/react-native-gestures/navigation/MainNavigator.jsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import TapGesture from "../screens/TapGesture.jsx";
 import LongPressGesture from "../screens/LongPressGesture.jsx";
 import FlingGesture from "../screens/FlingGesture.jsx";
+import PanGesture from "../screens/PanGesture.jsx";
 
 
 export default function MainNavigator() {
@@ -13,6 +14,7 @@ export default function MainNavigator() {
             <Drawer.Screen name="TapGesture" component={TapGesture} options={{ title: 'Tap Gesture' }} />
             <Drawer.Screen name="LongPressGesture" component={LongPressGesture} options={{ title: 'Long Press' }} />
             <Drawer.Screen name="FlingGesture" component={FlingGesture} options={{ title: 'Fling Gesture' }} />
+            <Drawer.Screen name="PanGesture" component={PanGesture} options={{ title: 'Pan Gesture' }} />
    
         </Drawer.Navigator>
     );
